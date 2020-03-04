6547b2d38ee195649e412d6cbf9c6dc7b8bd8384
longteng <475493503@qq.com>
2020-03-04 21:18:56 +0800

    增加轮播图

--- index.js
+++ index.js
@@ -16,6 +16,52 @@ $(window).scroll(function(){
 $(".elevator .return").click(function(){
     $("html,body").animate({scrollTop:0},500)
 })
+
+// 轮播图
+var carouselObj = new Carousel({
+    box: $('.carousel .banner-con'),
+    img: $('.carousel .banner-item'),
+    dots: $('.carousel .banner-con .dots li'),
+    prev: $(".carousel .banner-con .prev"),
+    next: $(".carousel .banner-con .next"),
+    intervalTime: 3000,
+    gradualTime: 600,
+    className:"active",
+})
+// function changeImg(index){
+//     $(".carousel .banner-item").eq(index).fadeIn();
+//     $(".carousel .banner-item.active").fadeOut()
+//     $(".carousel .banner-item.active").removeClass("active");
+//     $(".carousel .banner-item").eq(index).addClass("active")
+    
+//     $(".carousel .dots li.active").removeClass("active");
+//     $(".carousel .dots li").eq(index).addClass("active")
+// }
+
+// var index=0;
+// function carousel(n){
+//     if (index < $(".carousel .banner-item").length - 1) {
+//         index+=n;
+//     } else {
+//         index = 0;
+//     }
+//     changeImg(index)
+// }
+
+// $(".banner-con .btn").click(function(){
+//     if($(this).hasClass("prev")){
+//         carousel(-1)
+//     }else{
+//         carousel(1)
+//     }
+    
+// })
+
+// $(".carousel .dots li").click(function(){
+//     index=$(this).index();
+//     changeImg(index)
+// })
+
  //点击添加类的函数
  function addOneClass(ele,className){
     ele.click(function(){
@@ -25,8 +71,7 @@ $(".elevator .return").click(function(){
         $(this).addClass(className)
     })
 }
-// 轮播图dots点击
-addOneClass($(".carousel .banner-con .dots li"),"active")
+
 // 学习路线nav点击
 addOneClass($(".learningRoute .nav-item"),"active")
 // 热门课程nav点击
